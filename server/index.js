63385814caff8cae400c83f16463e124b363ce98
baohng <bao.hng221b@gmail.com>
2023-10-15 00:09:33 +0700

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';  // package for using .env file
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`Connected to MongoDB`);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log(`Unconnected to MongoDB`);
+  });
 
 const PORT = 3000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!!!`);
+  console.log(`Server is running on port ${PORT}!`);
   }
-)
+);
