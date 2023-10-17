b93ddff5676780169fd3b6eefe6f6f432fd00a0b
baohng <bao.hng221b@gmail.com>
2023-10-17 15:51:25 +0700

    create a sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   // destructuring the req.body to save to database
@@ -18,3 +20,27 @@ export const signup = async (req, res, next) => {
   }
 
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found!'));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, 'Wrong credential!'));
+    }
+    const token = jwt.sign({ id: validUser.id }, process.env.JWT_SECRET);
+    const {password: pass , ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { HttpOnLy: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
